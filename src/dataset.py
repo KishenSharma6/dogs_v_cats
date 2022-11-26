ad59bfa2c83bff4224ee366abd94fd1d7996d02d
KishenSharma6 <ksharma6@proton.me>
2022-11-26 13:26:12 -0800

    Added standardize_image_name function to rename image files in training set

---
+++ src/dataset.py
@@ -0,0 +1,49 @@
+import torch
+import cv2 #used to read images
+
+import numpy as np
+import os
+
+
+class ImageDataset:
+    def __init__(self, image_paths, targets, augmentations = None) -> None:
+        self.image_paths = image_paths
+        self.targets = targets
+        self.augmentations = augmentations #torch vision transforms will suffice
+
+    def __len__(self):
+        return len(self.targets)
+    
+    def __getitem__(self, idx):
+        target = self.targets[idx]
+        image = cv2.imread(self.image_paths + str(idx) + '.jpg')
+        #openCV loads image channels as BGR, we want to convert that to RGB 
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        #check for augmentations, apply changes to image if true
+        if self.augmentations is not None:
+            augmented = self.augmentations(image = image)
+            image = augmented["image"]
+        
+        #convert image tensors to be in channel first format
+        image = np.transpose(image, (2,0,1)).astype(np.float32)
+
+        return {
+            "image": torch.tensor(image),
+            "target": torch.tensor(target)
+        }
+
+
+def standardize_image_name(path):
+    """
+    convert image names to numeric names
+    """
+    cat_idx = 0
+    dog_idx = 12_500
+    for img in os.listdir(path):
+        if img.startswith('cat.'):    
+            os.rename(os.path.join(path, img), os.path.join(path, str(cat_idx) + ".jpg"))
+            cat_idx +=1
+        if img.startswith('dog.'):    
+            os.rename(os.path.join(path, img), os.path.join(path, str(dog_idx) + ".jpg"))
+            dog_idx +=1
