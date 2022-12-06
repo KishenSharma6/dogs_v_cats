41d208bb6790540b5862dc76fe2e508ea9262e1d
KishenSharma6 <ksharma6@proton.me>
2022-12-05 16:56:10 -0800

    refactoring

--- src/dataset.py
+++ src/dataset.py
@@ -1,15 +1,19 @@
-import torch
+import torch, torchvision
+import torchvision.transforms.functional as fn
+
 import PIL #used to read images
 
 import numpy as np
-import os
+import os,random
 
 
 class ImageDataset:
     def __init__(self, image_paths, targets, augmentations = None) -> None:
         self.image_paths = image_paths
         self.targets = targets
-        self.augmentations = augmentations #torch vision transforms will suffice
+        self.augmentations = augmentations
+        # self.mean = self.calculate_mean()
+        # self.std = self.calculate_std()
 
     def __len__(self):
         return len(self.targets)
@@ -18,26 +22,43 @@ class ImageDataset:
         target = self.targets[idx]
         image = PIL.Image.open(self.image_paths + str(idx) + '.jpg')
 
+        #convert PIL to tensor (channel first)
+        image = torchvision.transforms.functional.pil_to_tensor(image)
+        image = image.type(torch.float64)
+
         #check for augmentations, apply changes to image if true
         if self.augmentations is not None:
             augmented = self.augmentations(image)
-            image = augmented
-        
-        #convert image tensors to be in channel first format
-        image = np.transpose(image, (2,0,1)).astype(np.float64)
+            image = augmented        
 
         return {
             "image": torch.tensor(image),
             "target": torch.tensor(target)
         }
 
+    def calculate_mean(self):
+        mean = 0
+        for i in range(len(self.targets)):
+            mean += torch.mean(self.__getitem__(i)['image'], dim= [1,2])
 
-def standardize_image_name(path):
+        return mean/len(self.targets)
+        
+    def calculate_std(self):
+        std = 0
+        for i in range(len(self.targets)):
+            std += torch.std(self.__getitem__(i)['image'], dim = [1,2])
+
+        return std/len(self.targets)
+
+
+
+
+def standardize_image_name(path, cat_start_idx= 0, dog_start_idx= 12_500):
     """
     convert image names to numeric names
     """
-    cat_idx = 0
-    dog_idx = 12_500
+    cat_idx = cat_start_idx
+    dog_idx = dog_start_idx
     for img in os.listdir(path):
         if img.startswith('cat.'):    
             os.rename(os.path.join(path, img), os.path.join(path, str(cat_idx) + ".jpg"))
@@ -45,3 +66,24 @@ def standardize_image_name(path):
         if img.startswith('dog.'):    
             os.rename(os.path.join(path, img), os.path.join(path, str(dog_idx) + ".jpg"))
             dog_idx +=1
+
+def rename_jpg(path_to_read, path_to_write):
+    files = os.listdir(path_to_read)
+
+    for i, file in enumerate(files):
+        os.rename(os.path.join(path_to_read, file), os.path.join(path_to_write, ''.join([str(i), '.jpg'])))
+    print("rename complete")
+
+
+def calculate_mean(training_data):
+    mean = 0
+    for i in range(len(training_data.targets)):
+        mean += torch.mean(training_data.__getitem__(i)['image'], dim= [1,2])
+    return mean/len(training_data.targets)
+
+        
+def calculate_std(training_data):
+    std = 0
+    for i in range(len(training_data.targets)):
+        std += torch.std(training_data.__getitem__(i)['image'], dim = [1,2])
+    return std/len(training_data.targets)
